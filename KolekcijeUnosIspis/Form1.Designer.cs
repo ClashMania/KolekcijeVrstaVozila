32190c1f00d8228e1ec5df437947ce476796a612
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-02-15 09:25:04 +0100

    Add files via upload

---
+++ KolekcijeUnosIspis/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace KolekcijeUnosIspis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.cmbxVrsta = new System.Windows.Forms.ComboBox();
+            this.btnUnos = new System.Windows.Forms.Button();
+            this.btnIspisi = new System.Windows.Forms.Button();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(64, 15);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(175, 20);
+            this.txtMarka.TabIndex = 0;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(64, 87);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(175, 20);
+            this.txtModel.TabIndex = 1;
+            // 
+            // cmbxVrsta
+            // 
+            this.cmbxVrsta.FormattingEnabled = true;
+            this.cmbxVrsta.Items.AddRange(new object[] {
+            "Automobil",
+            "Avion",
+            "Brod"});
+            this.cmbxVrsta.Location = new System.Drawing.Point(64, 155);
+            this.cmbxVrsta.Name = "cmbxVrsta";
+            this.cmbxVrsta.Size = new System.Drawing.Size(175, 21);
+            this.cmbxVrsta.TabIndex = 2;
+            this.cmbxVrsta.SelectedIndexChanged += new System.EventHandler(this.cmbxVrsta_SelectedIndexChanged);
+            // 
+            // btnUnos
+            // 
+            this.btnUnos.Location = new System.Drawing.Point(16, 218);
+            this.btnUnos.Name = "btnUnos";
+            this.btnUnos.Size = new System.Drawing.Size(145, 70);
+            this.btnUnos.TabIndex = 3;
+            this.btnUnos.Text = "Unos";
+            this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.btnUnos_Click);
+            // 
+            // btnIspisi
+            // 
+            this.btnIspisi.Location = new System.Drawing.Point(720, 218);
+            this.btnIspisi.Name = "btnIspisi";
+            this.btnIspisi.Size = new System.Drawing.Size(148, 70);
+            this.btnIspisi.TabIndex = 4;
+            this.btnIspisi.Text = "Ispisi";
+            this.btnIspisi.UseVisualStyleBackColor = true;
+            this.btnIspisi.Click += new System.EventHandler(this.btnIspisi_Click);
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(265, 15);
+            this.txtIspis.Multiline = true;
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.ReadOnly = true;
+            this.txtIspis.Size = new System.Drawing.Size(603, 161);
+            this.txtIspis.TabIndex = 5;
+            this.txtIspis.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.Location = new System.Drawing.Point(375, 218);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(108, 70);
+            this.btnObradi.TabIndex = 6;
+            this.btnObradi.Text = "Obradi";
+            this.btnObradi.UseVisualStyleBackColor = true;
+            this.btnObradi.Click += new System.EventHandler(this.btnObradi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Vrsta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Marka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Model";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 309);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnObradi);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.btnIspisi);
+            this.Controls.Add(this.btnUnos);
+            this.Controls.Add(this.cmbxVrsta);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMarka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.ComboBox cmbxVrsta;
+        private System.Windows.Forms.Button btnIspisi;
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Button btnUnos;
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ KolekcijeUnosIspis/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace KolekcijeUnosIspis
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        List<Vozilo> voziloList = new List<Vozilo>();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+        class Vozilo
+        {
+            string marka, model, vrsta;
+            string voziPo;
+
+            public string VoziPo { get => voziPo; set => voziPo = value; }
+            public string Marka { get => marka; set => marka = value; }
+            public string Model { get => model; set => model = value; }
+            public string Vrsta { get => vrsta; set => vrsta = value; }
+
+            public Vozilo(string marka, string model, string vrsta)
+            {
+                this.marka = marka;
+                this.model = model;
+                this.vrsta = vrsta;
+
+            }
+
+            public override string ToString()
+            {
+                return "Marka: " + this.marka + "\nModel" + this.model + "\nVrsta: " + this.vrsta + "\nVozi po: " + this.voziPo + "\n\r";
+            }
+
+
+
+
+        }
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (txtMarka.Text == "" ||
+                    txtModel.Text == "" ||
+                    cmbxVrsta.Text == "")
+                {
+
+                    Vozilo vozilo = new Vozilo(txtMarka.Text, txtModel.Text, cmbxVrsta.Text);
+                    voziloList.Add(vozilo);
+                    txtModel.Clear();
+                    txtMarka.Clear();
+                }
+
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Pogresan unos, pokusajte opet", "Pogresan Unos", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+
+
+        }
+
+        private void btnIspisi_Click(object sender, EventArgs e)
+        {
+            foreach (Vozilo v in voziloList)
+            {
+                txtIspis.AppendText(v.ToString());
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            foreach (Vozilo v in voziloList)
+            {
+                if (v.Vrsta == "Avion")
+                {
+                    v.VoziPo = "Zrak";
+                }
+                else if (v.Vrsta == "Automobil")
+                {
+                    v.VoziPo = "Cesta";
+                }
+                else
+                {
+                    v.VoziPo = "Voda";
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbxVrsta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
